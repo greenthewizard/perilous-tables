156aeee7f6c38c6a8b7641525e70e104c94045fd
buddytherat <buddytherat@gmail.com>
2018-05-17 22:29:05 -0500

    Added monster generator test

--- src/index.js
+++ src/index.js
@@ -1,15 +1,38 @@
 import './js/styles.js';
 import perilous from './js/perilous.js'
 
-const appendTable = () => {
-    perilous.table('tbl:creature/creature[base]')
-    .then(res => {
-        const $p = document.createElement('p');
-        $p.textContent = res;
-    
-        document.querySelector('#roll-div')
-        .appendChild($p);
+document.addEventListener("DOMContentLoaded", function() {
+    const $btn = document.querySelector('#btn--gen');
+    $btn.addEventListener('click', (e) => {
+        perilous.table('tbl:creature/creature[base]')
+        .then(res => {
+            document.querySelector('#creature .table')
+            .textContent = res;
+        });
+        perilous.table('tbl:detail[activity]')
+        .then(res => {
+            document.querySelector('#activity .table')
+            .textContent = res;
+        });
+        perilous.table('tbl:detail[alignment]')
+        .then(res => {
+            document.querySelector('#alignment .table')
+            .textContent = res;
+        });
+        perilous.table('tbl:detail[disposition]')
+        .then(res => {
+            document.querySelector('#disposition .table')
+            .textContent = res;
+        });
+        perilous.table('tbl:detail[no. appearing]')
+        .then(res => {
+            document.querySelector('#no-appearing .table')
+            .textContent = res;
+        });
+        perilous.table('tbl:detail[size]')
+        .then(res => {
+            document.querySelector('#size .table')
+            .textContent = res;
+        });
     });
-}
-
-setInterval(appendTable, 100);
+});
